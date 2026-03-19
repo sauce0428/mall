a5536222517b7db83ccc5baa0d85fd55c3191b9c
KTH <sauce0428@gmail.com>
2026-03-19 09:45:52 +0900

    리스트페이지 완료

--- src/pages/todo/ListPage.jsx
+++ src/pages/todo/ListPage.jsx
@@ -1,6 +1,7 @@
 import "./ListPage.css";
 import Header from "../../include/Header";
 import { useSearchParams } from "react-router-dom";
+import ListComponent from "../../components/todo/ListComponent";
 
 const ListPage = () => {
   const [queryParam] = useSearchParams();
@@ -11,15 +12,10 @@ const ListPage = () => {
     <>
       <div className="main-container">
         <Header />
-
-        <p>
-          Todo List page={page} size={size}
-        </p>
-        <main className="content-area">
-          <div className="button-wrapper">
-            <button className="custom-btn-outline" type="button">
-              List Page
-            </button>
+        <main className="list-content-area">
+          <div className="list-wrapper">
+            {/* 실제 데이터 목록이 표시되는 컴포넌트 */}
+            <ListComponent />
           </div>
         </main>
       </div>
