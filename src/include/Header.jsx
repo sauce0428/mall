f37a4c419742bc8c6483b063a8da321059de8982
KTH <sauce0428@gmail.com>
2026-03-19 09:45:30 +0900

    세팅완료

--- src/include/Header.jsx
+++ src/include/Header.jsx
@@ -1,12 +1,20 @@
 import "./Header.css";
 import { useState } from "react";
 import { Link } from "react-router-dom";
+import { useSelector } from "react-redux";
 
 export default function Header() {
   const [isDropdownOpen, setIsDropdownOpen] = useState(false);
+  const [isProductDropdownOpen, setIsProductDropdownOpen] = useState(false);
+  const loginState = useSelector((state) => state.loginSlice);
 
   const toggleDropdown = () => {
     setIsDropdownOpen(!isDropdownOpen);
+    setIsProductDropdownOpen(false);
+  };
+  const toggleProductDropdown = () => {
+    setIsProductDropdownOpen(!isProductDropdownOpen);
+    setIsDropdownOpen(false);
   };
 
   return (
@@ -21,38 +29,74 @@ export default function Header() {
               ABOUT
             </Link>
 
-            {/* 드롭다운 영역 */}
-            <div className="nav-dropdown">
-              <button className="dropdown-toggle" onClick={toggleDropdown}>
-                TODO<span className="arrow">▾</span>
-              </button>
+            {/* todo 드롭다운 영역 */}
+            {loginState.email ? (
+              <>
+                <div className="nav-dropdown">
+                  <button className="dropdown-toggle" onClick={toggleDropdown}>
+                    TODO<span className="arrow">▾</span>
+                  </button>
 
-              {isDropdownOpen && (
-                <ul className="dropdown-menu">
-                  <li>
-                    <Link to="/todo/list">LIST</Link>
-                  </li>
-                  <li>
-                    <Link to="/todo/add">ADD</Link>
-                  </li>
-                  <li>
-                    <Link to="/todo/modify">MODIFY</Link>
-                  </li>
-                  <li>
-                    <Link to="/todo/read/20">READ</Link>
-                  </li>
-                  <li className="divider"></li>
-                  <li>
-                    <Link to="#">예비용</Link>
-                  </li>
-                </ul>
-              )}
-            </div>
+                  {isDropdownOpen && (
+                    <ul className="dropdown-menu">
+                      <li>
+                        <Link to="/todo/list">LIST</Link>
+                      </li>
+                      <li>
+                        <Link to="/todo/add">ADD</Link>
+                      </li>
+                      <li>
+                        <Link to="/todo/modify">MODIFY</Link>
+                      </li>
+                      <li>
+                        <Link to="/todo/read/20">READ</Link>
+                      </li>
+                      <li className="divider"></li>
+                      <li>
+                        <Link to="#">예비용</Link>
+                      </li>
+                    </ul>
+                  )}
+                </div>
+                {/* product 드롭다운 영역 */}
+                <div className="nav-dropdown">
+                  <button
+                    className="dropdown-toggle"
+                    onClick={toggleProductDropdown}
+                  >
+                    PRODUCT <span className="arrow">▾</span>
+                  </button>
+
+                  {isProductDropdownOpen && (
+                    <ul className="dropdown-menu">
+                      <li>
+                        <Link to="/product/list">LIST</Link>
+                      </li>
+                      <li>
+                        <Link to="/product/add">ADD</Link>
+                      </li>
+                      <li className="divider"></li>
+                      <li>
+                        <a href="#">예비용</a>
+                      </li>
+                    </ul>
+                  )}
+                </div>
+              </>
+            ) : (
+              <></>
+            )}
           </div>
           <div className="nav-right">
-            <Link to="/login" className="nav-link">
-              Login
-            </Link>
+            {!loginState.email ? (
+              <Link to="/member/login" className="login-link">
+                Login
+              </Link>
+            ) : (
+              <Link to="/member/logout" className="login-link">
+                Logout
+              </Link>
+            )}
           </div>
         </div>
       </nav>
--- src/main.jsx
+++ src/main.jsx
@@ -1,5 +1,18 @@
+import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
-import App from "./App.jsx";
+import App from "./App";
+import { Provider } from "react-redux";
+import store from "./store";
 
-ReactDOM.createRoot(document.getElementById("root")).render(<App />);
+// 1. root 요소를 한 번만 생성합니다.
+const root = ReactDOM.createRoot(document.getElementById("root"));
+
+// 2. Provider로 App을 감싸서 한 번만 렌더링합니다.
+root.render(
+  <React.StrictMode>
+    <Provider store={store}>
+      <App />
+    </Provider>
+  </React.StrictMode>,
+);
